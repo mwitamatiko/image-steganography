38756c6bac24fc0e5ae8d82ef123bc43dae77fe5
mwitamatiko <63615394+mwitamatiko@users.noreply.github.com>
2020-09-22 23:58:47 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
